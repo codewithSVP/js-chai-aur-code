13aa991bce9ef707a8498d7ba49b6eaf097f84d8
Shivansh Patel <119845133+codewithSVP@users.noreply.github.com>
2025-02-09 16:08:35 +0000

    Datatype Conversion Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,4 +29,37 @@ eg. 1 => true, 0 => false, random num => true
 
 let someNum = 33
 let stringOfNum = String(someNum)
-console.log(stringOfNum, typeof(stringOfNum))
+// console.log(stringOfNum, typeof(stringOfNum))
+
+// ******************OPERATIONS******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+/* ARITHMETIC OPERATIONS
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+*/
+
+let str1 = "Hello"
+let str2 = " SVP"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2) // auto conversion of 2nd arg to string
+// console.log(1 + "2") // auto conversion of 1st arg to string
+// console.log("1" + 2 + 2) // because 1st arg was string, it auto converted everything to string
+// console.log(1 + 2 + "2") // here, because addition of args was possible, so last arg which is a string was appended to the string of sum of first 2 args
+
+let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2 (NOT a good way to write variables)
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
