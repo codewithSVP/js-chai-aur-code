dbc3b9e407c9f6dec0d16f9335ee9cfdb94851e2
Shivansh Patel <119845133+codewithSVP@users.noreply.github.com>
2025-02-17 09:50:34 +0000

    While & Do-while loop discussed

---
+++ 05_iterators/02_while.js
@@ -0,0 +1,22 @@
+// while and do-while loop
+
+// While Loop: Entry-controlled Loop
+let i = 0
+// while (i <= 10) {
+//     console.log(`Value of i: ${i}`)
+//     i+=2
+// }
+
+let myArr = ["batman", "ironman", "spiderman"]
+let index = 0
+// while (index < myArr.length) {
+//     console.log(`Value: ${myArr[index]}`)
+//     index++
+// }
+
+// Do-while: Exit-controlled Loop
+let score = 13
+do {
+    console.log(`Score is: ${score}`)
+    score++
+} while(score <= 10);
